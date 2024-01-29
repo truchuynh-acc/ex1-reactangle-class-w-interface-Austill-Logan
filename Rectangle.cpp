ea18bb568a66b8f70d55269f9504b5968778680d
Austill-Logan <156864806+Austill-Logan@users.noreply.github.com>
2024-01-28 18:43:53 -0600

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,61 +1,53 @@
-/**Driver code
-Programmer: Logan Austill
-Project number: 1
-Project Desc: Rectangle Class Interface
-Course: COSC 2436 PF III Data Structures
-Date:   01/20/2024
-*/
+/**Rectangle.cpp Code
+ Programmer: Logan Austill
+ Project number: 1
+ Project Desc: Rectangle Class Interface
+ Course: COSC 2436 PF III Data Structures
+ Date:   01/20/2024
+ */
 
-#include <iostream>
 #include "Rectangle.h"
-using namespace std;
 
 /*
- main driver program for rectangle class.
+ default constructor for Rectangle objects.
  */
-int main(){
-    
-    /*
-     l = length
-     w = width
-     */
-    double l, w;
-    
-    /*
-     creation of Rectangle object named theRectangle.
-     */
-    Rectangle theRectangle;
-    
-    /*
-     printing to screen the initial state of the rectangle.
-     */
-    cout<< "Initial state of the rectangle" << endl;
-    cout<< "    length = " << theRectangle.getLength() << endl;
-    cout<< "    width = " << theRectangle.getWidth() << endl;
-    cout<< "    area = " << theRectangle.getArea() << endl << endl;
-    
-    /*
-     prompting user for length and width inputs.
-     */
-    cout<< "Please enter length of rectangle: " << endl;
-    cin>>l;
-    cout<< "Please enter width of rectangle: " << endl;
-    cin>>w;
-    
-    /*
-     updating the rectangle with user dimensions.
-     */
-    theRectangle.set(l, w);
-    
-    /*
-     printing to screen new dimensions and area using get methods.
-     */
-    cout<< "" << endl;
-    cout << "Current state of the Rectangle" << endl;
-    cout << "   length = " << theRectangle.getLength() << endl;
-    cout << "   width = " << theRectangle.getWidth() << endl;
-    cout << "   area    = " << theRectangle.getArea() << endl << endl;
-    
-    return 0;
-    
+Rectangle::Rectangle(){
+    length = 1.0;
+    width = 1.0;
+}
+
+/*
+ constructor for Rectangle object.
+ */
+bool Rectangle::set(double newLength, double newWidth){
+    if (newLength && newWidth > 0.0){
+        length = newLength;
+        width = newWidth;
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+/*
+ getter returns length.
+ */
+double Rectangle::getLength() const{
+    return length;
+}
+
+/*
+ getter returns width.
+ */
+double Rectangle::getWidth() const{
+    return width;
+}
+
+/*
+ getter for area
+ calculates area then returns solution.
+ */
+double Rectangle::getArea() const{
+    return length * width;
 }
